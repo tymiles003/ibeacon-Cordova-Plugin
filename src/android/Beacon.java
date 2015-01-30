e42f00cb61d23283ce7fe89925056805f2059702
derozgur <o.cimen@triodor.eu>
2015-01-30 10:48:35 +0200

    Android

---
+++ src/android/Beacon.java
@@ -0,0 +1,62 @@
+package com.biotekno.beacon;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.util.Log;
+
+import com.oksijen.lbs.indoorpositioning.proximity.VFProximityBeacon;
+import com.oksijen.lbs.indoorpositioning.proximity.VFProximityManager;
+
+public class Beacon extends CordovaPlugin{
+    
+
+    private boolean initialized = false ;
+    public static final String tag = "biobeacon";   
+    private VFProximityManager vfm;    
+    
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView)  {
+       
+        super.initialize(cordova, webView);           
+        
+       
+        vfm = VFProximityManager.getInstance();
+        
+        initialized = true;  
+        Log.d(tag,"Biobeacon plugin initialized");
+    }
+    
+    
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {         
+        
+        if(action.equalsIgnoreCase("add_beacon")){
+            
+            String uuid = "";
+            int major = 0;
+            int minor = 0;
+            
+            try{
+                uuid = args.getString(0);
+                major = args.getInt(1);
+                minor = args.getInt(2);
+            }
+            catch(JSONException error){
+                callbackContext.error("Parameters error");
+                return false;
+            }
+            
+            vfm.addBeacon(new VFProximityBeacon(uuid,major,minor));
+        
+            return true;
+        }       
+                        
+        return false;  
+    } 
+    
+}
---
+++ www/biobeacon.js
@@ -0,0 +1,31 @@
+var argscheck = require('cordova/argscheck'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
+
+const PluginMethod ={ 
+	ADD_BEACON :"add_beacon"
+}
+
+function Biobeacon(){
+	console.log("Biobeacon.js contruct");
+};
+
+Biobeacon.prototype.addBeacon = function(beacon,onsucceed,onfail){	
+	cordova.exec(function(winParam) {
+		if(onsucceed) onsucceed(winParam);
+	},function(error) {
+		if(onfail) onfail(error);
+	},"Beacon",PluginMethod.ADD_BEACON,[beacon.uuid,beacon.major,beacon.minor]);
+}
+
+console.log("Beacon plugin added to module");
+
+
+module.exports = Biobeacon;
+
+
+/**
+* Biotekno Ibeacon Oksijen Cordova Plugin v1.0 2014
+*
+* @arthur Ozgur Cimen
+**/
